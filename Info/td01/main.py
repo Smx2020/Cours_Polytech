60f8ec25affccd3514184d5b95ed45c9a843627b
smx2020 <smx2020@gmail.com>
2017-09-18 21:06:17 +0200

    first commit

---
+++ Info/td01/main.py
@@ -0,0 +1,43 @@
+print("quotient de {0} par {1}: {2}".format(3124,6,3124 // 6))
+print("reste de {0} par {1}: {2}".format(3124,6,3124 % 6))
+print("type de 123*(1.56 -3): {0}".format(type(123*(1.56 -3))))
+print("valeur de 123*(1.56 -3): {0}".format(123*(1.56 -3)))
+
+
+def pizza(n):
+	print("Pate à pizza pour {0} personnes".format(n))
+	print("farine {0} g, sel {1} c. à cafè, levure {2} sachet, huile d'olive {3} c. à soupe, eau tiède {4} cl".format((350/4)*n,(2/4)*n,(1/4)*n,(3/4)*n,(25/4)*n))
+
+def quo_e(v1,v2,i = 0):
+	if v1 < v2:
+		print(i)
+		return(i)
+	else :
+		return(quo_e(v1-v2,v2,i+1))
+
+
+def division(v1,v2):
+	print("division euclidienne de {0} par {1} :".format(v1,v2))
+	q = quo_e(v1,v2)
+	print("{0} = {1} * {2} + {3}".format(v1, q, v2,v1 - q*v2))
+
+
+def sq_name(L,l,name):
+	for i in range(L):
+		for i in range(l):
+			print("*{0}".format(name),end = "")
+		print()
+
+pizza(4)
+pizza(10)
+
+division(83,4)
+
+sq_name(4,4,"Olivier")
+
+v1 = input("v1 = :")
+v2 = input("v2 = :")
+division(int(v1),int(v2))
+
+name = input("nom = ")
+sq_name(4,4,name)
---
+++ Info/td02/main.py
@@ -0,0 +1,38 @@
+prix = int(input("prix ? "))   #1
+print(("trop cher\nj'achète pas" if prix > 1001 else ("j'achète\nmême si c'est pas donné" if prix > 100 else "j'achète\nc'est donné!" )))
+print("fin des achats")   #
+
+etudiant = input("Nom Prenom Sport FC :")
+nom, prenom, sport, FC = etudiant.split()
+print("Entrainement de {} {} :".format(prenom, nom))
+if sport == "aucun" :
+	if FC > 70:
+		print("\tva falloir vous y mettre ...")
+	else :
+		print("\tbon coeur, entrainement inutile")
+elif sport == "endurance" or sport == "combat" or sport == "collectif" :
+	print("entrainez-vous à la fréquence max de ",int(FC) * (1.5 if sport == "collectif" else (1.2 if sport == "endurance" else 2)) )
+else:
+	print("je ne connais pas ce sport")
+
+#	[5]	(1<3 or (5<2 and 8>1) )  True
+
+#	[6]		a	|	b	|	and	|	or
+
+#			F	|	F	|	F	|	F
+#			F	|	T	|	F	|	T
+#			T	|	F	|	F	|	T
+#			T	|	T	|	T	|	T
+
+#	[7]		a and b		<=>		not( not(a) or not(b))
+
+
+def calendrier(mois):
+	if mois == 2:
+		print("Il y a 29 jours les années bissextiles et 28 jours sinon")
+	if mois < 8:
+		print("Il y a {} jours dans le mois {}".format(30 + mois % 2, mois))
+	elif mois < 13 :
+		print("Il y a {} jours dans le mois {}".format(30 + (mois + 1) % 2, mois))
+	else :
+		print("Erreur mois non compris entre 1 et 12")
