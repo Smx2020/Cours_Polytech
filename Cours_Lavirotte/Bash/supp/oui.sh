85357e61d86b0aa4c588a9d3eed9b969541e8fe6
smx2020 <smx2020@gmail.com>
2017-11-13 10:51:57 +0100

    ajout du debut td fichier

---
+++ Cours_Lavirotte/Bash/supp/oui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $1 in
+	"o")
+		echo "Yes Yes Yes"
+		;;
+	"O")
+		echo "Yes Yes Yes"
+		;;
+	"n")
+		echo "No No No"
+		;;
+	"N")
+		echo "No No No"
+		;;
+	*)
+		echo "Da fuck man"
+		;;
+esac
